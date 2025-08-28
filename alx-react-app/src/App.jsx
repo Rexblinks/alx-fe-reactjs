870cebbd4869bedbe0a603fc4fccc3db90096fa1
simon <simonodoh3@gmail.com>
2025-08-28 07:02:24 -0700

    corrected my import structure

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <h2>My React App</h2>
-      <WelcomeMessage />
-    </>
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
