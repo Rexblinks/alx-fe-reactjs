f866c6200cf7135f2054afb6506b64a72c6b9fce
simon <simonodoh3@gmail.com>
2025-09-07 09:26:39 -0700

    fix: update ProfilePage to use UserContext instead of props

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,7 +1,12 @@
-import UserInfo from "./UserInfo";
+import UserInfo from './UserInfo';
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 function ProfilePage() {
+  const userData = useContext(UserContext);
+
   return <UserInfo />;
 }
 
 export default ProfilePage;
+
