44e09b589886a2f5dd76df517c8fa48c7fce18c9
simon <simonodoh3@gmail.com>
2025-09-07 09:23:10 -0700

    refactor: replace prop drilling with Context API

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+// src/components/UserProfile.jsx
+
+import React from "react";
+
+// Functional component that takes props
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
