9ce3507e50fc30b31983b07d518aa223a4df7abb
simon <simonodoh3@gmail.com>
2025-09-30 09:21:00 -0700

    Fix: simplify UserProfile to directly use context

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import UserContext from "../UserContext";
 import UserInfo from './UserInfo';
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+      <UserInfo />
+    </div>
+  );
 }
 
 export default UserProfile;
