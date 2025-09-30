3e459e44508e7d45eaa6a4156ee6d549a2831083
simon <simonodoh3@gmail.com>
2025-09-30 09:26:56 -0700

    Fix: UserProfile minimal context usage

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,16 +1,13 @@
 import { useContext } from "react";
 import UserContext from "../UserContext";
-import UserInfo from './UserInfo';
 
 function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
     <div>
-      <h2>User Profile</h2>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
-      <UserInfo />
     </div>
   );
 }
