4d5999c49f610655265ce0f98c59e268dfcb5f99
simon <simonodoh3@gmail.com>
2025-10-06 02:03:38 -0700

    fix: rename FormikForm.jsx to formikForm.js for ALX checker

---
+++ form-handling-react/src/components/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import FormikForm from './components/formikForm'; // updated import
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
